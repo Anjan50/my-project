3754c374191d733debe54065bdb0f5c3c1f4bce6
Anjan Diyora <anjanjee11@gmail.com>
2022-06-10 10:13:26 +0530

    solved error

--- components/UrlShortnerBox.js
+++ components/UrlShortnerBox.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from "next/image"
 
 const UrlShortnerBox = () => {
     return (
@@ -13,7 +12,7 @@ const UrlShortnerBox = () => {
                     <div className="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
                         <div className="flex w-full justify-center items-end">
                             <div className="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
-                                <label for="hero-field" className="leading-7 text-sm text-gray-600">Enter URL</label>
+                                <label htmlFor="hero-field" className="leading-7 text-sm text-gray-600">Enter URL</label>
                                 <input type="text" id="hero-field" name="hero-field" className="w-full bg-gray-100 bg-opacity-50 rounded focus:ring-2 focus:ring-blue-200 focus:bg-transparent border border-gray-300 focus:border-blue-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                             </div>
                             <button className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Shorten URL</button>
